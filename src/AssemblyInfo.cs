11d222663f2fc69196e8dde06c0884c2564258fc
Morozov <Morozov@0af28e04-c486-454c-8ef5-decbcf0f22eb>
2007-04-13 13:05:00 +0000

    Добавил использование технологии ClickOnce для автоматического обновления программы.

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.CompilerServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 //
-[assembly: AssemblyTitle("")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("UEEditor")]
+[assembly: AssemblyDescription("Редактор нераспознанных выражений")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
+[assembly: AssemblyCompany("Inforoom")]
+[assembly: AssemblyProduct("UEEditor")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
