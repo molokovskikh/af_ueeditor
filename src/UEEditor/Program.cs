55cbd3c7b1f242abd4c072ba69b3851d5af16b72
Квасов Роман <r.kvasov@analit.net>
2010-09-01 18:11:30 +0400

    исправил логирование

--- src/UEEditor/Program.cs
+++ src/UEEditor/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Reflection;
 using log4net;
 using System.Windows.Forms;
 using Subway.Helpers;
@@ -19,10 +20,17 @@ namespace UEEditor
 
 			if (!Debugger.IsAttached)
 				AppDomain.CurrentDomain.UnhandledException += (sender, args) => {
-					SendMessageOnException(sender, (Exception)args.ExceptionObject);
+					ILog logger;
+					if (sender == null)
+						logger = LogManager.GetLogger(typeof(Program));
+					else
+						logger = LogManager.GetLogger(sender.GetType());
+					logger.Error(args.ExceptionObject);
+
 					MessageBox.Show("В приложении возникла необработанная ошибка.\r\nИнформация об ошибке была отправлена разработчику.");
 				};
 
+			GlobalContext.Properties["Version"] = Assembly.GetExecutingAssembly().GetName().Version;
 			//Эти две строчки есть в StatViewer'е, возможно, из-за одной из них не работает "корректное" отображение 
 			//значений столбца "Сегмент" в фильтрах компонентов DevExpress
 			//Application.EnableVisualStyles();
@@ -30,17 +38,6 @@ namespace UEEditor
 
 			InputLanguageHelper.SetToRussian();
 			Application.Run(new frmUEEMain());
-		}
-
-		public static void SendMessageOnException(object sender, Exception exception)
-		{
-			ILog logger;
-			if (sender == null)
-				logger = LogManager.GetLogger(typeof(Program));
-			else
-				logger = LogManager.GetLogger(sender.GetType());
-			logger.Error(exception);
-			Mailer.SendMessageToService(exception);
 		}
 	}
 }
